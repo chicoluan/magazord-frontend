837a970aea1acf78baaf532af2c1225358e49f9d
engluanoliv <engluanoliv@gmail.com>
2026-01-16 09:56:01 -0300

    feat: instance of axios to github api endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-function App() {
+export default function App() {
   return <h1>Magazord Frontend Github</h1>
 }
-
-export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from 'react'
+
+export default function HomePage(): JSX.Element {
+  return <h1>Home Page</h1>
+}
