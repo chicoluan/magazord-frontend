3d0f78361198e54ec2245a3a2b953e9dd575305a
engluanoliv <engluanoliv@gmail.com>
2026-01-16 13:51:41 -0300

    feat: add the statusbar and navbar components (later remove the status bar because is not neccessary) also import the incons from figma to be like the design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
 import type { JSX } from 'react'
+import { Github } from './ui/icons'
 
 export default function NavBar(): JSX.Element {
   return (
-    <div className="flex bg-background md:bg-primary">
-      <div className="flex items-center justify-between">
-        <p className="text-sm">15:18</p>
+    <div className="flex bg-primary p-6">
+      <div className="flex col-span-2 gap-4">
+        <Github className="size-6" />
+        <h1 className="text-primary-foreground">Github</h1>
+        <p className="text-primary-foreground">Profile</p>
       </div>
     </div>
   )
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router'
 
 export default function RootLayout() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-svh">
       <NavBar />
-      <div className="flex">
+      <div className="flex mx-6">
         <Outlet />
       </div>
     </div>
